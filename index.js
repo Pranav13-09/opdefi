a4e73ad74e2e5aa20472a30b13e922bfb5fad577
Pranav13-09 <morepranav51@gmail.com>
2023-08-16 02:34:51 +0530

    okk

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({extended : true}))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello, Express with TypeScript!');
+  res.send('Hello,Its working!');
 });
 
 app.listen(port, () => {
